1d2c5d603e01535d67f6d2a76fccc9542cdcc893
elahe <elahehseyedain93@gmail.com>
2023-08-10 16:55:50 +0100

    Importing html pages.

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('products/<product>', views.product_category)
+    path('', views.index, name="home"), #Home page
+    path('products/<product>', views.product_category), #Suits product category
+    path('signup', views.signup, name="signup"), #Signup page
+    path('shop', views.shop, name="shop"), #Shop page
+    path('contact', views.contact, name="contact"), #Contact page
+    path('profile', views.profile, name="profile"), #Profile page
+
 ]
--- products/views.py
+++ products/views.py
@@ -12,3 +12,18 @@ def product_category(request, product):
          return HttpResponse(f"Here is the list of our {product}.")
      else:
         return HttpResponse("The page you are looking for doesn't exist.")
+     
+def signup(request):
+    return render(request, "products/signup.html")
+
+     
+def shop(request):
+    return render(request, "products/shop.html")
+
+     
+def contact(request):
+    return render(request, "products/contact.html")
+
+     
+def profile(request):
+    return render(request, "products/profile.html")
