2f2c137887355b602147ba03ff5ac468c7d0c134
elahe <elahehseyedain93@gmail.com>
2023-08-11 16:42:45 +0100

    Creating model classes.

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Shirt
 
 # Register your models here.
+
+# add models to the admin panel
+admin.site.register(Shirt)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Shirt(models.Model):
+    title = models.CharField(max_length=70)
+    price = models.PositiveIntegerField()
+
+# In your script or interactive shell with command: python3 manage.py shell  
+from products.models import Shirt 
+new_shirt = Shirt(1,"Long_sleeve shirt", 20) 
+new_shirt.save()
+
+# Or If I don't want to store in a variable
+from products.models import Shirt 
+Shirt(2,"Sport polo shirt", 35).save()
