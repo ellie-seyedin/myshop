1d03ab47b80a3210dfcb07aa18af15dcda8162b0
elahe <elahehseyedain93@gmail.com>
2023-08-17 13:41:47 +0100

    Implementing manyTomany and oneTomany relation.

--- products/admin.py
+++ products/admin.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
-from .models import Shirt, Product, Brand
+from .models import Product, Brand, Address, Category
 
 # Register your models here.
 
 # add models to the admin panel
 class productAdmin(admin.ModelAdmin):
     readonly_fields = ("slug",)
-    list_display = ("title","id", "brand", "category", "is_best_seller")
+    list_display = ("title","id", "brand", "is_best_seller")
     list_filter = ("is_best_seller",)
     search_fields = ("title", "category", "brand",)
 
 admin.site.register(Brand)
-admin.site.register(Shirt)
+admin.site.register(Category)
 admin.site.register(Product, productAdmin)
+admin.site.register(Address)
--- products/models.py
+++ products/models.py
@@ -1,24 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Address(models.Model):
+   street = models.CharField(max_length=100)
+   zipcode = models.CharField(max_length=10)
+   city = models.CharField(max_length=20)
+   country = models.CharField(max_length=20)
+    
+   def __str__(self):
+      return f"{self.country} - {self.city}"
+
 class Brand(models.Model):
     title = models.CharField(max_length=70)
-    logo =models.ImageField()
+    logo =  models.ImageField()
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, null =True)
       
     def __str__(self):
       return f"{self.title}"
     
-class Shirt(models.Model):
+class Category(models.Model):
     title = models.CharField(max_length=70)
-    price = models.PositiveIntegerField()
-    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, null=True)
     description = models.TextField(blank=True)
-    is_best_seller = models.BooleanField(default=False)
+
+    def __str__(self):
+      return f"{self.title}"
     
 class Product(models.Model): 
     title = models.CharField(max_length=40)
     description = models.TextField(blank=True)
-    category = models.CharField(max_length=50)
+    category = models.ManyToManyField(Category)
     image = models.ImageField(blank=True, upload_to="upload-img")
     price = models.PositiveIntegerField()
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE, null=True, related_name="prd")
