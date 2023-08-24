d459ae01e98c0a7481e5595b64c0a7ebc23411ad
elahe <elahehseyedain93@gmail.com>
2023-08-24 12:49:24 +0100

    Customizing the form field.

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class FeedbackForm(forms.Form):
-    name = forms.CharField(required=True)
-    email = forms.EmailField()
+    name = forms.CharField(required=True, max_length=10, label="Full Name", help_text="Enter your name:", error_messages={"required": "You forgot to add your name", "max_length":"It's too long, make it shorter"})
+    email = forms.EmailField(required=True)
