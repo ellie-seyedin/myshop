3a6584027fed83b56aad00d760f3ba1be8f68ae5
elahe <elahehseyedain93@gmail.com>
2023-09-04 14:54:22 +0100

    Model forms.

--- products/forms.py
+++ products/forms.py
@@ -1,6 +1,14 @@
 from django import forms
+from .models import Feedback
 
-class FeedbackForm(forms.Form):
-    name = forms.CharField(required=True, error_messages={"required": "You forgot to add your name"})
-    rating = forms.IntegerField(min_value=1, max_value=5)
-    text = forms.CharField(label="Your feedback", widget=forms.Textarea, max_length=200)
+# class FeedbackForm(forms.Form):
+#     name = forms.CharField(required=True, error_messages={"required": "You forgot to add your name"})
+#     rating = forms.IntegerField(min_value=1, max_value=5)
+#     text = forms.CharField(label="Your feedback", widget=forms.Textarea, max_length=200)
+
+
+class Feedback(forms.ModelForm):
+    class Meta:
+        model = Feedback
+        fields = ['name', 'rating', 'text']
+        labels = {'name': 'Full Name', 'text':'Your feedback'}
